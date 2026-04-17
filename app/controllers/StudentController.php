a2988d77737d7c7121fb71fc99d6edb8b2649535
Carryn <carryn.002@ski.sch.id>
2026-04-17 11:08:31 +0700

    feat: implement student index page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -14,9 +14,10 @@ class StudentController extends Controller
         $studentModel = new Student();
         $students = $studentModel->getStudents();
 
-        print_r($students);
 
-        $this->view('students.index');
+        $this->view('students.index',[
+            'students' => $students
+        ]);
     }
 
 
