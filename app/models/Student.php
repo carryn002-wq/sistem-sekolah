edf7f20ac34122d6dbf111b3f3310e9552673e3c
Carryn <carryn.002@ski.sch.id>
2026-04-17 11:35:53 +0700

    feat: implement student detail page

--- app/models/Student.php
+++ app/models/Student.php
@@ -26,6 +26,22 @@ class student extends Database
         return $students;
     }
 
+    // Menampilkan Detail Siswa
+    public function getStudent(int $id)
+    {
+        $query = "SELECT * FROM {$this->table} WHERE id = ?";
+
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param('i',$id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $student = $result->fetch_assoc();
+
+        return $student;
+    }
+
 }
 
 
