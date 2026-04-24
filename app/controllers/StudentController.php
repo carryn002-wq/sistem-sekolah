9c967d73598bf25157e40fa6b063eb5cbca615b9
Carryn <carryn.002@ski.sch.id>
2026-04-24 11:34:38 +0700

    feat: implement edit and delete student

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -30,23 +30,42 @@ class StudentController extends Controller
 public function show(string $id)
 {
     $id = intval($id);
-
     $studentModel = new Student();
     $student = $studentModel->getStudent($id);
-    $this->view('students.show', [
-        'student' => $student
-    ]);
+    $this->view('students.show', ['student' => $student]);
 }
 
 public function edit(string $id) 
 {
-    $this->view('students.edit');
+    $id = intval($id);
+    $studentModel = new Student();
+    $student = $studentModel->getStudent($id);
+
+    $this->view('students.edit', [
+        'student' => $student
+    ]);
+    
 }
 
 public function store()
 {
     $studentModel = new Student();
     $studentModel->insert($_POST);
+}
+
+
+public function update(string $id)
+{
+    $id = intval($id);
+    $studentModel = new Student();
+    $studentModel->update($_POST, $id);
+}
+
+public function destroy(string $id)
+{
+    $id = intval($id);
+    $studentModel = new Student();
+    $studentModel->delete($id);
 }
 
 }
